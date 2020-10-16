baef159fa4e56649de775bda6db8d8d72403efe7
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:27:53 +0700

    Program Aritmatika

---
+++ src/pbo2/pkg10116374/latihan12/aritmatika/PBO210116374Latihan12Aritmatika.java
@@ -0,0 +1,32 @@
+package pbo2.pkg10116374.latihan12.aritmatika;
+
+/** @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk melakukan Proses Aritmatika
+ */
+public class PBO210116374Latihan12Aritmatika {
+
+   
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println(" a + b = " + (a+b));
+        System.out.println(" a - b = " + (a-b));
+        System.out.println(" a * b = " + (a*b));
+        System.out.println(" b / a = " + (b/a));
+        System.out.println(" b % a = " + (b%a));
+        System.out.println(" c % a = " + (c%a));
+        System.out.println(" a++ = " + (a++));
+        System.out.println(" b-- = " + (a--));
+        System.out.println(" d++ = " + (d++));
+        System.out.println(" ++d = " + (++d));
+
+        
+    }
+    
+}
